d999762feb33efc4103856ec6f7d17de9441fc5c
lzl32947g <lzl32947@gmail.com>
2018-05-02 15:40:30 +0800

    完善了部分UI

--- app/src/main/java/cn/edu/lzu/oss/ecab/activities/MainActivity.java
+++ app/src/main/java/cn/edu/lzu/oss/ecab/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package cn.edu.lzu.oss.ecab.activities;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.util.Log;
@@ -9,6 +11,10 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.baidu.mapapi.SDKInitializer;
 import com.flyco.tablayout.SegmentTabLayout;
@@ -21,13 +27,15 @@ import cn.edu.lzu.oss.ecab.R;
 import cn.edu.lzu.oss.ecab.fragment.ItemFragment;
 import cn.edu.lzu.oss.ecab.fragment.MapFragment;
 import cn.edu.lzu.oss.ecab.interfaces.FragmentInterface;
+import cn.edu.lzu.oss.ecab.util.WindowUtil;
 import cn.edu.lzu.oss.ecab.view.BaseViewPager;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private ImageView sendImage;
+    private ImageView receiveImage;
     private BaseViewPager viewPager;
     private List<Fragment> fragments;
-    private SegmentTabLayout segmentTabLayout;
     private FragmentInterface backInterfaceListener;
     private boolean isActive = false;
 
@@ -35,18 +43,18 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initSDK();
+        WindowUtil.initWindows(this);
         setContentView(R.layout.activity_main);
-        initAll();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        initViews();
+
     }
 
     private void initSDK() {
         SDKInitializer.initialize(getApplicationContext());
     }
 
-    private void initAll() {
+    private void initViews() {
         fragments = new ArrayList<>();
         fragments.add(MapFragment.newInstance());
         fragments.add(ItemFragment.newInstance());
@@ -63,20 +71,23 @@ public class MainActivity extends AppCompatActivity
                 return fragments == null ? 0 : fragments.size();
             }
         });
-        segmentTabLayout = findViewById(R.id.seg_tab);
-        segmentTabLayout.setTabData(new String[]{"寄件", "取件"});
-        segmentTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
-            @Override
-            public void onTabSelect(int position) {
-                backInterfaceListener.onFragmentBackPress();
-                viewPager.setCurrentItem(position);
-            }
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-            @Override
-            public void onTabReselect(int position) {
-
+        sendImage = findViewById(R.id.image_send);
+        receiveImage = findViewById(R.id.image_receive);
+        List<ImageView> button = new ArrayList<>();
+        button.add(sendImage);
+        button.add(receiveImage);
+        int width = (int)(WindowUtil.getWIDTH() / 2.5);
+        if (width != 0) {
+            for (ImageView x:button){
+                ViewGroup.LayoutParams params = x.getLayoutParams();
+                params.width = width;
+                x.setLayoutParams(params);
             }
-        });
+        }
+
     }
 
     @Override
@@ -84,12 +95,12 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else if (isActive && backInterfaceListener != null){
+        } else if (isActive && backInterfaceListener != null) {
             boolean result = backInterfaceListener.onFragmentBackPress();
-            if (!result){
+            if (!result) {
                 super.onBackPressed();
             }
-        }else{
+        } else {
             super.onBackPressed();
         }
     }
--- app/src/main/java/cn/edu/lzu/oss/ecab/fragment/MapFragment.java
+++ app/src/main/java/cn/edu/lzu/oss/ecab/fragment/MapFragment.java
@@ -14,9 +14,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.baidu.location.BDAbstractLocationListener;
@@ -26,9 +29,12 @@ import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapPoi;
+import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
 import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
@@ -43,6 +49,7 @@ import java.util.List;
 import cn.edu.lzu.oss.ecab.activities.MainActivity;
 import cn.edu.lzu.oss.ecab.R;
 import cn.edu.lzu.oss.ecab.interfaces.FragmentInterface;
+import cn.edu.lzu.oss.ecab.util.WindowUtil;
 
 import static android.support.v4.content.ContextCompat.checkSelfPermission;
 import static cn.edu.lzu.oss.ecab.util.Const.MapFragmentPermission.BAIDU_READ_PHONE_STATE;
@@ -51,32 +58,49 @@ public class MapFragment extends Fragment implements FragmentInterface {
     private MapView mMapView;
     private BaiduMap mBaiduMap;
     private SlidingUpPanelLayout panelLayout;
+    private ImageView userImage;
+    private ImageView locationImage;
+    private CardView searchCard;
+
     private List<LatLng> points;
     private Context context;
-    public LocationClient mLocationClient = null;
     private MyLocationData locData;
-    public BDAbstractLocationListener myListener = new BDAbstractLocationListener() {
+    private LocationClient mLocationClient;
+    private MyBDAbstractLocationListener mLocationListener;
+    private boolean isFirstLocation = true;
+
+
+    public class MyBDAbstractLocationListener extends BDAbstractLocationListener {
         @Override
         public void onReceiveLocation(BDLocation bdLocation) {
             locData = new MyLocationData.Builder()
                     .accuracy(bdLocation.getRadius())
-                    .direction(100).latitude(bdLocation.getLatitude())
-                    .longitude(bdLocation.getLongitude()).build();
-
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100)
+                    .latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLongitude())
+                    .build();
             mBaiduMap.setMyLocationData(locData);
-            MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(1.0f);// 设置地图放大比例
-            mBaiduMap.setMapStatus(msu);
-
+            if (isFirstLocation) {
+                //获取经纬度
+                LatLng ll = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+                MapStatusUpdate status = MapStatusUpdateFactory.newLatLng(ll);
+                //mBaiduMap.setMapStatus(status);//直接到中间
+                mBaiduMap.animateMapStatus(status);//动画的方式到中间
+                isFirstLocation = false;
+                Log.i("位置：", bdLocation.getAddrStr() == null ? "null" : bdLocation.getAddrStr());
+            }
         }
-    };
+    }
+
     public MapFragment() {
     }
 
-    public static MapFragment newInstance(){
+    public static MapFragment newInstance() {
         return new MapFragment();
     }
 
-    public SlidingUpPanelLayout getPanel(){
+    public SlidingUpPanelLayout getPanel() {
         return panelLayout;
     }
 
@@ -87,76 +111,124 @@ public class MapFragment extends Fragment implements FragmentInterface {
         context = getActivity();
         initMap(view);
         initPanel(view);
-
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        userImage = view.findViewById(R.id.image_user);
+        locationImage = view.findViewById(R.id.image_now_location);
+        List<ImageView> button = new ArrayList<>();
+        button.add(userImage);
+        button.add(locationImage);
+        int length = WindowUtil.getWIDTH() / 6;
+        if (length != 0){
+            for (ImageView x : button){
+                ViewGroup.LayoutParams params = x.getLayoutParams();
+                params.width = length;
+                params.height = length;
+                x.setLayoutParams(params);
+            }
+        }
+
+        searchCard = view.findViewById(R.id.search_map_cardview);
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams)searchCard.getLayoutParams();
+        params.width = WindowUtil.getWIDTH() == 0 ? 240 : WindowUtil.getWIDTH() / 3 * 2;
+        params.height = 120;
+        params.topMargin = 320;
+        searchCard.setLayoutParams(params);
+    }
+
+    private void initLocation() {
+        mLocationClient = new LocationClient(getContext().getApplicationContext());
+        mLocationListener = new MyBDAbstractLocationListener();
+        /*注册监听*/
+        mLocationClient.registerLocationListener(mLocationListener);
+        /*配置定位*/
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setCoorType("bd09ll");
+        option.setScanSpan(1000);
+        option.setOpenGps(true);
+        option.setLocationNotify(true);
+        option.setIgnoreKillProcess(true);
+        option.SetIgnoreCacheException(false);
+        option.setWifiCacheTimeOut(5 * 60 * 1000);
+        option.setEnableSimulateGps(false);
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
     }
 
     private void initPanel(View view) {
         panelLayout = view.findViewById(R.id.sliding_layout);
-
     }
 
 
     private void initMap(View view) {
         mMapView = view.findViewById(R.id.baidu_maps);
+        mMapView.removeViewAt(1);
         mBaiduMap = mMapView.getMap();
+        MapStatus mMapStatus = new MapStatus.Builder().zoom(18).build();
+        MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mMapStatus);
+        mBaiduMap.setMapStatus(mMapStatusUpdate);
         points = new ArrayList<>();
         initPoints();
-        for (LatLng x:points){
+        for (LatLng x : points) {
             setMarker(x);
         }
         mBaiduMap.setMyLocationEnabled(true);
+        BaiduMap.OnMapClickListener listener = new BaiduMap.OnMapClickListener() {
+            public void onMapClick(LatLng point){
+                if (panelLayout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED){
+                    panelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+                }
+            }
+            public boolean onMapPoiClick(MapPoi poi){
+                return false;
+            }
+        };
+        mBaiduMap.setOnMapClickListener(listener);
+        BaiduMap.OnMarkerClickListener markerListener = new BaiduMap.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker){
+                Log.i("id:",marker.getId());
+                panelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+                return true;
+            }
+        };
+        mBaiduMap.setOnMarkerClickListener(markerListener);
+        mMapView.showScaleControl(false);
+        mMapView.showZoomControls(false);
         initPosition();
     }
 
     private void initPosition() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if(ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED ||
-                    ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED ||
-                    ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
-                MapFragment.this.requestPermissions(new String[]{ Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_PHONE_STATE },BAIDU_READ_PHONE_STATE );
-
-        }else {
-            locate();
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                MapFragment.this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_PHONE_STATE}, BAIDU_READ_PHONE_STATE);
+            else {
+                initLocation();
+            }
+        } else {
+            initLocation();
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == BAIDU_READ_PHONE_STATE){
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                locate();
+        if (requestCode == BAIDU_READ_PHONE_STATE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initLocation();
             }
-        }else {
-            Toast.makeText(getActivity(), "获取权限失败",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getActivity(), "获取权限失败", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void locate() {
-        mLocationClient = new LocationClient(getActivity().getApplicationContext());     //声明LocationClient类
-        mLocationClient.registerLocationListener(myListener);    //注册监听函数
-        LocationClientOption option = new LocationClientOption();
-        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
-        int span = 1000;
-        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
-        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
-        option.setOpenGps(true);//可选，默认false,设置是否使用gps
-        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
-        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
-        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
-        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
-        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
-        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
-        mLocationClient.setLocOption(option);
-        //开启定位
-
-        mLocationClient.start();
-    }
-
     private void setMarker(LatLng latLng) {
-        View v = View.inflate(getContext(),R.layout.fragment_map_marker , null);
+        View v = View.inflate(getContext(), R.layout.fragment_map_marker, null);
         BitmapDescriptor bitmap = BitmapDescriptorFactory.fromView(v);
         OverlayOptions option = new MarkerOptions().position(latLng).icon(bitmap);
         mBaiduMap.addOverlay(option);
@@ -170,19 +242,19 @@ public class MapFragment extends Fragment implements FragmentInterface {
 
     @Override
     public boolean onFragmentBackPress() {
-        if (panelLayout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED){
+        if (panelLayout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED) {
             panelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
             return true;
-        }else
+        } else
             return false;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof MainActivity){
-            ((MainActivity)context).setBackInterfaceListener(this);
-            ((MainActivity)context).setActive(true);
+        if (context instanceof MainActivity) {
+            ((MainActivity) context).setBackInterfaceListener(this);
+            ((MainActivity) context).setActive(true);
         }
     }
 
@@ -190,11 +262,11 @@ public class MapFragment extends Fragment implements FragmentInterface {
     public void onDetach() {
         super.onDetach();
         Context context = getContext();
-        if (context instanceof MainActivity){
-            ((MainActivity)context).setBackInterfaceListener(null);
-            ((MainActivity)context).setActive(false);
+        if (context instanceof MainActivity) {
+            ((MainActivity) context).setBackInterfaceListener(null);
+            ((MainActivity) context).setActive(false);
         }
-        if (mLocationClient != null){
+        if (mLocationClient != null) {
             mLocationClient.stop();
         }
     }
