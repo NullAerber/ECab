2feea78be50ed5f9af2da96fa296f7620adc1e30
NullAerber <1019735081@qq.com>
2018-05-16 10:05:01 +0800

    加入MQTT组件和代码，尚未调试

--- app/src/main/java/cn/edu/lzu/oss/ecab/fragment/MapFragment.java
+++ app/src/main/java/cn/edu/lzu/oss/ecab/fragment/MapFragment.java
@@ -1,26 +1,25 @@
 package cn.edu.lzu.oss.ecab.fragment;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -47,16 +46,23 @@ import com.baidu.mapapi.model.LatLng;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+import cn.edu.lzu.oss.ecab.activities.HistoryActivity;
 import cn.edu.lzu.oss.ecab.activities.MainActivity;
 import cn.edu.lzu.oss.ecab.R;
-import cn.edu.lzu.oss.ecab.activities.PayActivity;
 import cn.edu.lzu.oss.ecab.activities.SaveActivity;
 import cn.edu.lzu.oss.ecab.interfaces.BackPressInterface;
 import cn.edu.lzu.oss.ecab.interfaces.UserClickInterface;
+import cn.edu.lzu.oss.ecab.util.ComponentSetting;
+import cn.edu.lzu.oss.ecab.util.LinkerServer;
 import cn.edu.lzu.oss.ecab.util.WindowUtil;
+import cn.edu.lzu.oss.ecab.util.mqtt.MyMqtt;
 
 import static cn.edu.lzu.oss.ecab.util.Const.MapFragmentPermission.BAIDU_READ_PHONE_STATE;
 
@@ -79,6 +85,34 @@ public class MapFragment extends Fragment implements BackPressInterface {
 
     private boolean clickMarker = false;
 
+//    /**
+//     * Map Hidden 中的相关控件
+//     */
+//    ImageView add_1;
+//    ImageView add_2;
+//    TextView length;
+//    TextView width;
+//    TextView weight;
+
+
+    /**
+     * MQTT相关参数
+     **/
+    private MyMqtt myMqtt;
+    //    private static String host = "iot.eclipse.org";//主机地址
+    private static String host = "123.206.127.199";//主机地址
+    private static String port = "1883";//MQTT端口(一般为1883)
+    private static String userID = "";//用户ID(无可以不填)
+    private static String passWord = "";//用户密码(无可以不填)
+    private static String clientID = UUID.randomUUID().toString();//随机生成字符串，放置clientID冲突
+    private static String TAG = "MapFragment";
+    private static String SubTopic = "EBToApp";
+    private static String PubTopic = "EBFromApp";
+    private List<String> list_longitude;
+    private List<String> list_latitude;
+    private ArrayAdapter adapter;//列表视图填充器
+    private List<String> message = new ArrayList<>();//消息
+
 
     public MapFragment() {
     }
@@ -96,17 +130,22 @@ public class MapFragment extends Fragment implements BackPressInterface {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         context = getActivity();
+
+        initMqtt();
+        getLocation();
+
         initMap(view);
         initPanel(view);
         initView(view);
         initPosition();
         setImageListener(view);
+
         return view;
     }
 
     private void setImageListener(View view) {
-        ImageView add_1 = view.findViewById(R.id.image_add_1);
-        ImageView add_2 = view.findViewById(R.id.image_add_2);
+        ImageView add_1 = view.findViewById(R.id.image_add_length);
+        ImageView add_2 = view.findViewById(R.id.image_add_width);
         ImageView add_3 = view.findViewById(R.id.image_add_3);
         final TextView length = view.findViewById(R.id.textview_length_number);
         final TextView width = view.findViewById(R.id.textview_width_number);
@@ -115,11 +154,25 @@ public class MapFragment extends Fragment implements BackPressInterface {
             @Override
             public void onClick(View view) {
                 switch (view.getId()) {
-                    case R.id.image_add_1:
-                        length.setText("2");
+                    case R.id.image_add_length:
+                        if (Integer.valueOf(length.getText().toString()) > 10)
+                            break;
+                        length.setText(Integer.valueOf(length.getText().toString()) + 1);
                         break;
-                    case R.id.image_add_2:
-                        width.setText("2");
+                    case R.id.image_add_width:
+                        if (Integer.valueOf(width.getText().toString()) > 10)
+                            break;
+                        width.setText(Integer.valueOf(width.getText().toString()) + 1);
+                        break;
+                    case R.id.image_minus_length:
+                        if (Integer.valueOf(length.getText().toString()) == 0)
+                            break;
+                        length.setText(Integer.valueOf(length.getText().toString()) - 1);
+                        break;
+                    case R.id.image_minus_width:
+                        if (Integer.valueOf(width.getText().toString()) == 0)
+                            break;
+                        width.setText(Integer.valueOf(width.getText().toString()) - 1);
                         break;
                     case R.id.image_add_3:
                         break;
@@ -131,10 +184,11 @@ public class MapFragment extends Fragment implements BackPressInterface {
         add_1.setOnClickListener(imageListener);
         add_2.setOnClickListener(imageListener);
         add_3.setOnClickListener(imageListener);
-        ImageView imageView = view.findViewById(R.id.imageView8);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        ImageView map_ok = view.findViewById(R.id.map_ok);
+        map_ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getPubMsg(length.getText().toString(), width.getText().toString(),view);
                 Intent intent = new Intent(getActivity(), SaveActivity.class);
                 startActivity(intent);
             }
@@ -244,6 +298,15 @@ public class MapFragment extends Fragment implements BackPressInterface {
         searchCard.setLayoutParams(params);
     }
 
+//    private void initMapHiddenView(View view) {
+//        add_1 = view.findViewById(R.id.image_add_1);
+//        add_2 = view.findViewById(R.id.image_add_2);
+//        add_3 = view.findViewById(R.id.image_add_3);
+//        length = view.findViewById(R.id.textview_length_number);
+//        width = view.findViewById(R.id.textview_width_number);
+//        weight = view.findViewById(R.id.textview_weight_number);
+//    }
+
     public class MyBDAbstractLocationListener extends BDAbstractLocationListener {
         @Override
         public void onReceiveLocation(BDLocation bdLocation) {
@@ -293,7 +356,6 @@ public class MapFragment extends Fragment implements BackPressInterface {
     private void initPanel(View view) {
         panelLayout = view.findViewById(R.id.sliding_layout);
     }
-
 
     private void initPosition() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -362,5 +424,93 @@ public class MapFragment extends Fragment implements BackPressInterface {
         if (mLocationClient != null) {
             mLocationClient.stop();
         }
+    }
+
+    /* Method created by Aerber Zhou
+     */
+    @SuppressLint("HandlerLeak")
+    private void initMqtt() {
+        myMqtt = new MyMqtt(new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == ComponentSetting.MQTT_STATE_CONNECTED) {
+                    //当Mqtt连接成功时
+                    Toast.makeText(getContext(), "连接成功", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "连接成功");
+                } else if (msg.what == ComponentSetting.MQTT_STATE_LOST) {
+                    //当丢失连接后
+                    Toast.makeText(getContext(), "连接丢失，进行重连", Toast.LENGTH_SHORT).show();
+                } else if (msg.what == ComponentSetting.MQTT_STATE_FAIL) {
+                    //当连接失败时
+                    Toast.makeText(getContext(), "连接失败", Toast.LENGTH_SHORT).show();
+                } else if (msg.what == ComponentSetting.MQTT_STATE_RECEIVE) {
+                    //当主线程收到数据后做什么
+                    Bundle bundle = msg.getData();
+                    message.add(bundle.getString(ComponentSetting.TOPIC) + ":" + bundle.getString(ComponentSetting.MESSAGE));//向List添加数据
+                    adapter.notifyDataSetChanged();//调用Adapter的notifyDataSetChanged，更改列表的显示
+                }
+                super.handleMessage(msg);
+            }
+        });
+
+        myMqtt.setMqttSetting(host, port, userID, passWord, clientID);//设置当前Mqtt连接信息
+        myMqtt.connectMqtt();
+    }
+
+    /* Method created by Aerber Zhou
+     */
+    private void getPubMsg(final String height, final String width, final View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<NameValuePair> params = new ArrayList<>();
+                params.add(new BasicNameValuePair("height", height));
+                params.add(new BasicNameValuePair("width", width));
+                LinkerServer linkerServer = new LinkerServer("get_pub_msg", params);
+                if (linkerServer.Linker()) {
+                    String response = linkerServer.getResponse();
+                    String[] str_record = response.split("\\|");
+                    for (int i = 0; i < str_record.length; ++i) {
+                        String[] record = str_record[i].split(";");
+                        myMqtt.pubMsg(record[0], record[1], 1);//发送消息
+                    }
+                    Snackbar.make(view, "已经成功", Snackbar.LENGTH_LONG)
+                            .setAction("查看详情", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent intent = new Intent(getContext(), HistoryActivity.class);
+//                                    intent.putExtra(getString(R.string.URL), id);
+                                    getContext().startActivity(intent);
+                                }
+                            }).show();
+                } else {
+                    Toast.makeText(getContext(), R.string.REQUEST_FAIL, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).start();
+    }
+
+    /* Method created by Aerber Zhou
+     */
+    private void getLocation() {
+        list_longitude = new ArrayList<>();
+        list_latitude = new ArrayList<>();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LinkerServer linkerServer = new LinkerServer("get_location");
+                if (linkerServer.Linker()) {
+                    String response = linkerServer.getResponse();
+                    String[] str_record = response.split("\\|");
+                    for (int i = 0; i < str_record.length; ++i) {
+                        String[] record = str_record[i].split(";");
+                        list_longitude.add(record[1]);
+                        list_latitude.add(record[2]);
+                    }
+                } else {
+                    Toast.makeText(getContext(), R.string.REQUEST_FAIL, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).start();
     }
 }
